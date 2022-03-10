5d422449f2f6c5f2c9c6f02bf7c27ce2331f7889
Nivaldo Farias <badivia.f@gmail.com>
2022-03-10 16:19:03 -0300

    Added minimum structure and styling to project

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(props) {
+  return (
+    <header>
+      <p>{props.text}</p>
+    </header>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import "./index.scss";
 import ReactDOM from "react-dom";
 import Main from "./Main";
 import List from "./List";
+import Header from "./Header";
 
 const topics = [1, 2, 3, 4, 5, 6];
 
 function Lista() {
   return (
     <div>
+      <Header text="Instagram" />
       <Main text="REACT" />
       <List topics={topics} />
     </div>
